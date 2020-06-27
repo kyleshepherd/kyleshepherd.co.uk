8cb1a40f08f3d0f60c8a65205ac24c64f14e8544
Kyle Shepherd <kyleshepherd13@gmail.com>
2020-06-27 18:03:24 +0100

    Contact completed

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import PageHeader from './PageHeader'
+import './Contact.css'
 
 function Contact () {
   return (
-    <h1>Contact</h1>
+    <React.Fragment>
+      <PageHeader pageTitle="Contact"/>
+      <div className="contactContent">
+        <p>If you'd like to get in touch, please send an email to <a
+          href="mailto:kyleshepherddev@gmail.com">kyleshepherddev@gmail.com</a> and I'll get back to you ASAP!</p>
+      </div>
+    </React.Fragment>
   )
 }
 
