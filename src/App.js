13f30ed95c59e384731bef3edc2d53d5f329ccc5
Kyle Shepherd <kyleshepherd13@gmail.com>
2020-06-27 15:01:10 +0100

    Created Header, Menu and routing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
+import Header from './components/Header'
+import About from './components/About'
+import Work from './components/Work'
+import Contact from './components/Contact'
+import Home from './components/Home'
 
-function App() {
+function App () {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header/>
+
+        <Switch>
+          <Route path="/about">
+            <About/>
+          </Route>
+          <Route path="/work">
+            <Work/>
+          </Route>
+          <Route path="/contact">
+            <Contact/>
+          </Route>
+          <Route path="/">
+            <Home/>
+          </Route>
+        </Switch>
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Header.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import './Header.css'
+import { Link } from 'react-router-dom'
+import Menu from './Menu'
+
+function Header () {
+
+  const [showMenu, setShowMenu] = useState(false)
+
+  const handleClose = () => setShowMenu(false)
+  const handleOpen = () => setShowMenu(true)
+
+  return (
+    <React.Fragment>
+      <header>
+        <div className="headerContainer">
+          <div className="siteTitle">
+            <h1>
+              <Link to="/">
+                Kyle Shepherd
+              </Link>
+            </h1>
+          </div>
+
+          <div className="navBtn" onClick={handleOpen}/>
+        </div>
+      </header>
+      <Menu isOpen={showMenu} closeMenu={handleClose}/>
+    </React.Fragment>
+  )
+}
+
+export default Header
