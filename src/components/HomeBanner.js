3e50ca606d2e5018a9cb238c4c538681a813f78a
Kyle Shepherd <kyleshepherd13@gmail.com>
2020-08-22 16:32:03 +0100

    Home page changes

--- src/components/HomeBanner.js
+++ src/components/HomeBanner.js
@@ -1,25 +1,30 @@
 import React from 'react'
 import './HomeBanner.css'
 
-function HomeBanner () {
-  return (
-    <div className="homeBanner">
-      <h2 className="role">Game and Web<br></br>Developer</h2>
-      <h3>
-        <span>Interested in</span><br></br>
-        Gameplay and UI Programming<br></br>
-        Frontend Web Development
-      </h3>
-      <h3>
-        <span>Based in</span><br></br>
-        Falmouth, UK
-      </h3>
-      <h3>
-        <span>Currently</span><br></br>
-        Junior Web Developer at Builtbycactus
-      </h3>
-    </div>
-  )
+function HomeBanner() {
+	return (
+		<div className="homeBanner">
+			<h2 className="role">
+				Web and Game<br></br>Developer
+			</h2>
+			<h3>
+				<span>Interested in</span>
+				<br></br>
+				Frontend Web Development<br></br>
+				Gameplay and UI Programming
+			</h3>
+			<h3>
+				<span>Based in</span>
+				<br></br>
+				Falmouth, UK
+			</h3>
+			<h3>
+				<span>Currently</span>
+				<br></br>
+				Web Developer at Builtbycactus
+			</h3>
+		</div>
+	)
 }
 
 export default HomeBanner
