6f66e2610fd375d49a0e5b43076ebfe0f02c55a6
Kyle Shepherd <kyle@thisissoon.com>
2021-11-07 11:26:19 +0000

    feat(Update): General update of some bits + bobs

--- src/components/About.js
+++ src/components/About.js
@@ -1,50 +1,59 @@
-import React from 'react'
-import PageHeader from './PageHeader'
-import './About.css'
+import React from "react";
+import PageHeader from "./PageHeader";
+import "./About.css";
 
-function About () {
+function About() {
   return (
     <React.Fragment>
-      <PageHeader pageTitle="About"/>
+      <PageHeader pageTitle="About" />
       <div className="aboutContent">
         <div className="aboutText">
           <p>
-            I’m Kyle, a full stack web developer currently based in Falmouth, UK. I studied Games Development:
-            Programming at Falmouth University, graduating in 2019. During my time at university, I worked on 5 game
-            projects in interdisciplinary teams, these projects varied from first person puzzle games to couch
-            competitive multiplayer games to third person shooters. <br/><br/>
-            During my final year of university, I secured a web development internship with a local agency and proceeded
-            to work there full time after graduating, working on large scale Wordpress websites for a variety of
-            clients.
-            <br/><br/>
-            I'm currently at London-based digital agency SOON_, working on projects using Svelte and React as our main frontend frameworks.
+            I’m Kyle, a Front End Engineer currently based in Falmouth, UK. I
+            studied Games Development at Falmouth University, specialising in
+            Programming and graduating in 2019. During my time at university, I
+            worked on 5 game projects in interdisciplinary teams, these projects
+            varied from first person puzzle games to couch competitive
+            multiplayer games to third person shooters. <br />
+            <br />
+            During my final year of university, I moved into web development,
+            using online courses and tutorials to learn the basics before
+            starting an internship.
+            <br />
+            <br />
+            I'm currently at London-based digital agency SOON_, working on
+            projects using Svelte and React as our main frontend frameworks.
           </p>
 
           <div className="skillList">
             <h3>Skills & Technologies</h3>
             <ul>
-              <li>HTML/CSS/PHP/ES6</li>
-              <li>React.js + Gatsby.js</li>
               <li>Svelte</li>
+              <li>React.js</li>
+              <li>Go (a lil bit)</li>
+              <li>HTML/CSS/SCSS/PHP/ES6 (all that fun stuff!)</li>
               <li>Tailwind CSS</li>
-              <li>AWS</li>
-              <li>WordPress</li>
-              <li>Laravel</li>
-              <li>Version Control (Git + SVN)</li>
-              <li>Scrum + Agile</li>
-              <li>Trello</li>
+              <li>Version Control (Git + Subversion)</li>
+              <li>Agile</li>
             </ul>
           </div>
 
-          <a className="cvBtn" href="/Kyle-Shepherd-CV.pdf" target="_blank" rel="noopener">View my CV</a>
+          <a
+            className="cvBtn"
+            href="/Kyle-Shepherd-CV.pdf"
+            target="_blank"
+            rel="noopener"
+          >
+            View my CV
+          </a>
         </div>
 
         <div>
-          <img src="/images/me.jpeg" className="portrait" alt="Photo of Kyle"/>
+          <img src="/images/me.jpeg" className="portrait" alt="Kyle Shepherd" />
         </div>
       </div>
     </React.Fragment>
-  )
+  );
 }
 
-export default About
+export default About;
