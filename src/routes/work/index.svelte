c931946ce875f39361ed7dac725a8409501f6e38
Kyle Shepherd <kyle@thisissoon.com>
2022-06-27 17:09:28 +0100

    feat(Work): Add all work

--- src/routes/work/index.svelte
+++ src/routes/work/index.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import WorkBlock from "../../components/WorkBlock/WorkBlock.svelte";
   import PageHeader from "../../components/PageHeader/PageHeader.svelte";
+  import { workData } from "../../data/work.data";
 </script>
 
 <style lang="scss">
@@ -11,14 +12,26 @@
     display: flex;
     flex-direction: column;
     justify-content: center;
+
+    &--intro {
+      margin-bottom: 1.5rem;
+    }
   }
 </style>
 
 <div class="work">
   <PageHeader header="Work" />
-  <p>
+  <p class="work--intro">
     Below is a selection of projects I've worked on across work, personal
     projects, freelance and university.
   </p>
-  <WorkBlock title="Belstaff" />
+  {#each workData as project}
+    <WorkBlock
+      title={project.title}
+      image={project.image}
+      body={project.body}
+      repoLink={project.repoLink || ""}
+      projectLink={project.projectLink || ""}
+    />
+  {/each}
 </div>
