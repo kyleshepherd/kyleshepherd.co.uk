3e054c7e6f7992f492ae6934b74ea31b7c44cd7f
Kyle Shepherd <kyle@thisissoon.com>
2022-11-17 17:43:35 +0000

    feat(sveltekit): upgrade

---
+++ src/sanity.ts
@@ -0,0 +1,20 @@
+import type { SanityClient } from "@sanity/client";
+import sanityClient from "@sanity/client";
+
+let client: SanityClient;
+
+export const setClient = () => {
+  client = sanityClient({
+    projectId: "d3y2fj3e",
+    dataset: "production",
+    apiVersion: "2021-10-21",
+    useCdn: false,
+  });
+};
+
+export const getProjects = async () => {
+  if (!client) {
+    setClient();
+  }
+  return await client.fetch('*[_type == "project"]');
+};
