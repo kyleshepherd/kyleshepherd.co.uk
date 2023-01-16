89a96a3e23153d755c5d1e598886e91e21a74d2c
Kyle Shepherd <kyle@thisissoon.com>
2023-01-16 13:36:48 +0000

    feat(Work): move server count request to client

--- src/routes/work/+page.server.ts
+++ src/routes/work/+page.server.ts
@@ -2,21 +2,11 @@ import { error } from "@sveltejs/kit";
 import { getProjects } from "../../sanity";
 
 export const load = async () => {
-  let guildCount = 0;
-
-  const res = await fetch(
-    "https://tarkov-tk-server-count-d55njyagta-ew.a.run.app/count",
-  );
-  const data = await res.json();
-
-  guildCount = data?.count ? data.count : 0;
-
   const projects = await getProjects();
 
   if (projects) {
     return {
       projects,
-      guildCount,
     };
   }
 
--- src/routes/work/+page.svelte
+++ src/routes/work/+page.svelte
@@ -2,10 +2,22 @@
   import WorkBlock from "../../components/WorkBlock/WorkBlock.svelte";
   import PageHeader from "../../components/PageHeader/PageHeader.svelte";
   import type { PageData } from "./$types";
+  import { onMount } from "svelte";
 
   export let data: PageData;
 
-  $: ({ projects, guildCount } = data);
+  let guildCount: number;
+
+  $: ({ projects } = data);
+
+  onMount(async () => {
+    const res = await fetch(
+      "https://tarkov-tk-server-count-d55njyagta-ew.a.run.app/count",
+    );
+    const data = await res.json();
+
+    guildCount = data?.count ? data.count : 0;
+  });
 </script>
 
 <div class="flex flex-col justify-center flex-grow mb-8 basis-full">
