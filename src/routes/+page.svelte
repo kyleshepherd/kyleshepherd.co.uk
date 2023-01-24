f19e4a94409ff0ad18738f23012c6de51967e2b5
Kyle Shepherd <kyle@thisissoon.com>
2023-01-24 15:00:52 +0000

    feat(Home): remove listening to

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,6 @@
 <script lang="ts">
   import PageHeader from "../components/PageHeader/PageHeader.svelte";
   import DetailBlock from "../components/DetailBlock/DetailBlock.svelte";
-  import type { PageData } from "./$types";
-
-  export let data: PageData;
-
-  $: ({ track } = data);
 </script>
 
 <div class="flex flex-col justify-center flex-grow basis-full">
@@ -17,17 +12,9 @@
     />
   </div>
   <div class="mb-8">
-    <DetailBlock header="Building with" content="Svelte, React & Go" />
+    <DetailBlock header="Building with" content="Svelte & Go" />
   </div>
   <div class="mb-8">
     <DetailBlock header="Based in" content="London, UK" />
   </div>
-  {#if track}
-    <div class="mb-8">
-      <DetailBlock
-        header={track.nowPlaying ? "Listening to" : "Last listened to"}
-        content={track.title + " by " + track.artist}
-      />
-    </div>
-  {/if}
 </div>
