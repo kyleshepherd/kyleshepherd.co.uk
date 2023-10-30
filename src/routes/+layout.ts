0fd9156e7f6bd6a980d0a613c0feb485ada8b32a
Kyle Shepherd <kyle@thisissoon.com>
2023-10-30 10:12:17 +0000

    chore(Analytics): Add Vercel analytics

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,4 +1,8 @@
 import { setClient } from "../sanity";
+import { dev } from "$app/environment";
+import { inject } from "@vercel/analytics";
+
+inject({ mode: dev ? "development" : "production" });
 
 export const load = async () => {
   setClient();
