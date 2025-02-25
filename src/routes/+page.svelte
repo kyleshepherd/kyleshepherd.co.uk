c99c76db3f60fde7100807c72966b57a3a94af37
Kyle Shepherd <kyle@thisissoon.com>
2025-02-25 16:03:44 +0000

    repo setup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,2 @@
-<script lang="ts">
-  import PageHeader from "../components/PageHeader/PageHeader.svelte";
-  import DetailBlock from "../components/DetailBlock/DetailBlock.svelte";
-</script>
-
-<div class="flex flex-grow basis-full flex-col justify-center">
-  <PageHeader header="Senior Software Engineer" />
-  <div class="mb-8">
-    <DetailBlock
-      header="Currently"
-      content="Senior Software Engineer @ SOON_"
-    />
-  </div>
-  <div class="mb-8">
-    <DetailBlock header="Building with" content="Svelte & Go" />
-  </div>
-  <div class="mb-8">
-    <DetailBlock header="Based in" content="London, UK" />
-  </div>
-</div>
+<h1 class="font-display text-6xl">Kyle Shepherd</h1>
+<p class="text-6xl">Lorem ipsum dolor sit amet</p>
