cb33d60d87af3338adb76d15fbcc3c5f3305cc19
Kyle Shepherd <kyle@thisissoon.com>
2025-06-07 14:18:22 +0100

    fix(Header): Fix block link in header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,3 +1,5 @@
 <header class="xxs:p-3 sticky top-0 z-20 bg-black p-2 !pb-0 sm:px-5">
-	<a href="/" class="block border-b border-white/50 pb-3 font-serif text-4xl">Kyle Shepherd</a>
+	<div class="border-b border-white/50 pb-3">
+		<a href="/" class="font-serif text-4xl">Kyle Shepherd</a>
+	</div>
 </header>
