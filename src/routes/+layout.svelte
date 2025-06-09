5e155741b60da6721327743a95993b431f50f7e5
Kyle Shepherd <kyle@thisissoon.com>
2025-06-09 13:37:28 +0100

    feat(layout): remove console.log

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,14 +8,8 @@
 	let { children } = $props();
 
 	onMount(() => {
-		// Initialize Lenis
 		const lenis = new Lenis({
 			autoRaf: true,
-		});
-
-		// Listen for the scroll event and log the event data
-		lenis.on("scroll", e => {
-			console.log(e);
 		});
 	});
 </script>
