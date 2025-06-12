ff4c11260876fb5bad94ee61666f1c98c08b8f07
Kyle Shepherd <kyle@thisissoon.com>
2025-06-12 14:56:51 +0100

    feat(about): Update text

--- src/lib/components/About.svelte
+++ src/lib/components/About.svelte
@@ -2,8 +2,8 @@
 	<p class="max-w-prose text-lg xl:text-xl 2xl:text-2xl">
 		I'm <span class="font-mono">Kyle</span>, a Senior Software Engineer based in London with over
 		six years of experience working in agencies on projects for global clients, such as Tom Dixon,
-		White Cube and Belstaff. I specialise in Go and modern JavaScript frameworks like Svelte and
-		React.<br /><br />
+		White Cube and Belstaff. I specialise in modern JavaScript frameworks (Svelte/React), Go and
+		Node.js.<br /><br />
 		Currently, I work at
 		<a
 			href="https://thisissoon.com/"
@@ -14,8 +14,8 @@
 		development of several large-scale web applications using SvelteKit and React, and helped build
 		and maintain a suite of in-house services written in Go, including our custom CMS and image
 		processing tools. I work across the full stack, from frontend development to backend APIs, and
-		infrastructure, with deployments to Google Cloud Platform using tools like Terraform, and
-		Kubernetes.<br /><br />
+		infrastructure, with deployments to Google Cloud Platform and AWS using tools like Terraform,
+		and Kubernetes.<br /><br />
 		Alongside development, I'm involved in shaping features and functionality through planning and technical
 		design. I also support team growth through code reviews and mentoring junior developers.
 	</p>
